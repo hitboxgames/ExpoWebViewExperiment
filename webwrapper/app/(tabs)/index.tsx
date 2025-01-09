dc5444c1de690a02f8b73d72651c8ef861018c4a
Raul Faife <35008453+RaulFaife@users.noreply.github.com>
2025-01-09 15:11:17 -0800

    Hooked up Sitebrew and added loading state

--- webwrapper/app/(tabs)/index.tsx
+++ webwrapper/app/(tabs)/index.tsx
@@ -1,12 +1,52 @@
-import { View, SafeAreaView } from "react-native";
+import { View, ActivityIndicator } from "react-native";
 import WebView from "react-native-webview";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [url, setUrl] = useState("https://www.sitebrew.ai/");
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <View style={{ flex: 1, backgroundColor: "#eae7e0" }}>
-      <SafeAreaView />
-      <WebView source={{ uri: "https://www.lilsnack.co/" }} />
-      <View style={{ marginBottom: 10, backgroundColor: "white" }} />
+    <View
+      style={{
+        flex: 1,
+        paddingTop:
+          url === "https://www.sitebrew.ai/" ||
+          url === "https://www.sitebrew.ai/create"
+            ? 20
+            : 0,
+        backgroundColor:
+          url === "https://www.sitebrew.ai/" ||
+          url === "https://www.sitebrew.ai/create"
+            ? "white"
+            : "transparent",
+      }}
+    >
+      <WebView
+        source={{ uri: "https://www.sitebrew.ai/" }}
+        style={{ flex: 1 }}
+        onNavigationStateChange={(navState) => {
+          setUrl(navState.url);
+        }}
+        onLoadStart={() => setIsLoading(true)}
+        onLoadEnd={() => setIsLoading(false)}
+      />
+      {isLoading && (
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(255, 255, 255, 0.8)",
+          }}
+        >
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )}
     </View>
   );
 }
